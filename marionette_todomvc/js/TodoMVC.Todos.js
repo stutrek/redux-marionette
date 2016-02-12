f7df479e7ed374d18dd3fd4f2db280e9fbc68e72
Stu Kabakoff <stuart@compstak.com>
2016-02-12 13:39:25 -0500

    Revert "remove all changes that enable this so that it there can be a tutorial diff"
    This reverts commit 475bcabd9e98116600783754fb5f73d936c5d11a.

--- marionette_todomvc/js/TodoMVC.Todos.js
+++ marionette_todomvc/js/TodoMVC.Todos.js
@@ -18,6 +18,40 @@ var TodoMVC = TodoMVC || {};
 			Backbone.Model.prototype.initialize.call(this);
 		},
 
+		handleAction: function (action) {
+			if (action.id !== this.id) {
+				return;
+			}
+
+			switch (action.type) {
+				case 'EDIT_TODO':
+					this.set('text', action.text);
+					return;
+
+				case 'COMPLETE_TODO':
+					this.set('completed', !this.get('completed'));
+					return;
+
+				case 'DELETE_TODO':
+					this.destroy();
+			}
+		},
+
+		createAction: function (eventName) {
+			var action = this.toJSON();
+			switch (eventName) {
+				case 'change:text':
+					action.type = 'EDIT_TODO';
+					return action;
+
+				case 'change:completed':
+					action.type = 'COMPLETE_TODO'
+					return action;
+			}
+
+			return;
+		},
+
 		toggle: function () {
 			return this.set('completed', !this.isCompleted());
 		},
@@ -47,6 +81,49 @@ var TodoMVC = TodoMVC || {};
 		localStorage: new Backbone.LocalStorage('todos-backbone-marionette'),
 
 		comparator: 'created',
+
+		handleAction: function (action) {
+			switch (action.type) {
+				case 'ADD_TODO':
+					var id = action.id;
+					if (!id) {
+						var ids = this.pluck('id');
+						ids.push(0);
+						id = Math.max.apply(null, ids) + 1;
+					}
+					this.create({
+						text: action.text,
+						completed: action.completed,
+						id: id
+					});
+					break;
+
+				case 'CLEAR_COMPLETED':
+					var completed = this.getCompleted();
+					completed.forEach(function (todo) {
+						todo.destroy();
+					});
+
+			}
+		},
+
+		createAction: function (eventName, model) {
+			switch (eventName) {
+				case 'add':
+					return {
+						type: 'ADD_TODO',
+						text: model.get('text'),
+						id: model.get('id'),
+						completed: model.get('completed')
+					};
+
+				case 'remove':
+					return {
+						type: 'DELETE_TODO',
+						id: model.get('id')
+					};
+			}
+		},
 
 		getCompleted: function () {
 			return this.filter(this._isCompleted);
