0f8e31a5b14df8a1f7e636b54883635e0d6ca955
Stu Kabakoff <stuart@compstak.com>
2016-04-25 15:30:29 -0400

    make convenience function for wrapping marionette views

--- redux-marionette.js
+++ redux-marionette.js
@@ -4,8 +4,22 @@ import MarionetteComponent from './src/Component';
 
 var vent = {}
 
+function wrapView (MarionetteView, optionsFromProps) {
+	class WrappedView extends MarionetteComponent {
+		createMarionetteComponent (props) {
+			if (optionsFromProps) {
+				return new MarionetteView(optionsFromProps(props));
+			} else {
+				return new MarionetteView();
+			}
+		}
+	}
+	return WrappedView;
+}
+
 module.exports = {
 	marionetteMiddleware: middleware(vent),
 	marionetteDispatch: dispatch(vent),
-	MarionetteComponent: MarionetteComponent
+	MarionetteComponent,
+	wrapView
 };
