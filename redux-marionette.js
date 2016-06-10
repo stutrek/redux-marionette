9a76d2153b592dea1e843f2728c592a5dc1f1682
Stu Kabakoff <stuart@compstak.com>
2016-06-10 15:14:25 -0400

    check for updates to options in wrapped components

--- redux-marionette.js
+++ redux-marionette.js
@@ -1,11 +1,27 @@
 import middleware from './src/middleware';
 import dispatch from './src/dispatch';
 import MarionetteComponent from './src/Component';
+import _ from 'underscore';
 
 var vent = {}
 
 function wrapView (MarionetteView, optionsFromProps) {
+
 	class WrappedView extends MarionetteComponent {
+
+		shouldComponentUpdate (newProps) {
+			if (optionsFromProps) {
+				var newOptions = optionsFromProps(newProps);
+				if (!_.isEqual(this.marionetteComponent.options, newOptions)) {
+					return true
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		}
+
 		createMarionetteComponent (props) {
 			if (optionsFromProps) {
 				return new MarionetteView(optionsFromProps(props));
